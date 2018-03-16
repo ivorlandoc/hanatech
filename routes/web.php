2c2b3a79d342e00dddd440d20be0817f38df1711
Orlando Iparraguirre <iv.orlando.c@gmail.com>
2018-03-15 23:22:22 -0500

    r3

--- routes/web.php
+++ routes/web.php
@@ -140,7 +140,7 @@ Route::resource('mantestruct','ManteEstructurasController');
 
 
 
-Route::group(['prefix' => 'admin','namespace'=>'Admin', 'middleware' => 'admin', 'as' => 'admin.'], function () {
+Route::group(['prefix' => 'admin','namespace'=>'Admin', 'middleware' => 'admin'], function () {
     # User Management
         Route::group([ 'prefix' => 'users'], function () {
         Route::get('data', 'UsersController@data')->name('users.data');
