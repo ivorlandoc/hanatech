6199675d7173338f0a4984d68d22315e1c39151d
Orlando Iparraguirre <iv.orlando.c@gmail.com>
2018-03-15 23:02:22 -0500

    upcargo

--- app/Http/Controllers/Admin/CargosController.php
+++ app/Http/Controllers/Admin/CargosController.php
@@ -10,13 +10,12 @@ use DB;
 class CargosController extends Controller {
 
      public function index(Request $request){
-			$getAll = DB::table('cargo')
-            ->join('nivel', 'Nivel.IdNivel', '=', 'cargo.IdNivel')
-            ->join('tipocargo', 'tipocargo.IdTipo', '=', 'cargo.IdTipo')
-            ->select('cargo.*', 'tipocargo.Descripcion as TipoCargo', 'nivel.Descripcion as Nivel')
-            ->where('cargo.Descripcion', 'like', '%')
-            ->orderby('TipoCargo.Descripcion','DESC');//->paginate(10);
-            
+			$getAll = DB::table('cargo as c')
+            ->join('nivel as n', 'n.IdNivel', '=', 'c.IdNivel')
+            ->join('tipocargo as t', 't.IdTipo', '=', 'c.IdTipo')
+            ->select('t.Descripcion as TipoCargo', 'n.Descripcion as Nivel')
+            ->where('c.Descripcion', 'like', '%')
+            ->orderby('t.Descripcion','DESC');->paginate(10);            
             return view('admin.cargo.index',compact('getAll')); 
     }
 
